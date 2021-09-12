8c3a54692e4a7834b4374092d1f3ce88fc03eab5
huzigun <ungyul@gmail.com>
2021-09-12 22:37:35 +0900

    nuxtjs and nestjs init

---
+++ client/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12">
+      {{ hello }}
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'Index',
+  async asyncData({ $axios }) {
+    try {
+      const { data: hello } = await $axios.get('/');
+      return { hello };
+    } catch (error) {
+      console.dir(error);
+    }
+  },
+});
+</script>
