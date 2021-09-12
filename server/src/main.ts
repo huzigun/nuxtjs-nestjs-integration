708a263061d9c9d64a7bf53686aec4e14f0fa2d1
huzigun <ungyul@gmail.com>
2021-09-12 23:20:11 +0900

    Server hmr and logger init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: true, credentials: true });
   app.setGlobalPrefix('/api');
-  await app.listen(8706);
+
+  const port = process.env.PORT || 8706;
+
+  await app.listen(port);
+
+  console.log(`Server listening port: ${port}`);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
